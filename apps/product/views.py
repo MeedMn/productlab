76e21cbeff76e1d3c2526eb84a2e186242f3127d
Mohamed Menfalouti <mohamedmenfalouti1@gmail.com>
2023-04-20 10:34:27 +0000

    adding a shop view/url adding the shop page show products in the shop page working on paginations

--- apps/product/views.py
+++ apps/product/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from apps.product.models import Category, Product
 
 # Create your views here.
+
+def shop(request):
+    categories = Category.objects.all()
+    products1 = Product.objects.all()
+    page = request.GET.get('page', 1)
+    paginator = Paginator(products1, 12)
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
+        
+    return render(request,'shop.html',{"categories":categories,"products":products})
