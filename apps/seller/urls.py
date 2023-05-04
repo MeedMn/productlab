fd1d71f6283e48da11f831b5d1ee9c47c5983176
mjustatool <fousenaibdisbdis@gmail.com>
2023-05-04 14:29:35 +0100

    add modify/delete product(template/view/url) add edit delete buttons to dashboard

--- apps/seller/urls.py
+++ apps/seller/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path('Login_seller/',views.Login_seller,name='Login_seller'),
     path("dashboard/",views.dashboard,name="dashboard"),
     path('add_product/',views.add_product,name='add_product'),
+    path('update_product/<int:primaryKey>',views.updateProduct,name='updateProduct'),
+    path('deleteProduct/<int:primaryKey>',views.deleteProduct,name='deleteProduct'),
     path('logout/',auth_views.LogoutView.as_view(),name='logout'),
     path('login/',auth_views.LoginView.as_view(template_name='Login_seller.html'),name='login'),
 ]
--- apps/seller/views.py
+++ apps/seller/views.py
@@ -1,3 +1,4 @@
+import os
 from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import  render, redirect,get_object_or_404
 from django.contrib.auth import login, authenticate, update_session_auth_hash
@@ -104,3 +105,56 @@ def add_product(request):
         form = ProductForms()
         more_images = ProductImageForm()
     return render(request,'add_product.html',{'form':form,'more_images':more_images})
+
+
+@login_required(login_url='Login_seller')
+def updateProduct(request,primaryKey):
+    getProduct = get_object_or_404(Product,pk=primaryKey)
+    try:
+        os.remove("C:/Users/Snof_Bo/Desktop/Enviroment_Python/productlab/media/uploads/"+getProduct.image.name)
+        os.remove("C:/Users/Snof_Bo/Desktop/Enviroment_Python/productlab/media/"+getProduct.image.name)
+    except Exception as error :
+        print(error)
+        pass
+    if request.method == 'POST':
+        form = ProductForms(request.POST, request.FILES,instance=getProduct)
+        more_images = ProductImageForm(request.POST, request.FILES)
+        if (form.is_valid() and more_images.is_valid()) or form.is_valid():
+            if float(request.POST.get('price')) < 10000 and float(request.POST.get('price')) > 0 : 
+                product = form.save(commit=False)
+                img_to_delete = request.POST.getlist('delete')
+                for image in img_to_delete:
+                    print(image)
+                    ProductImage.objects.filter(image=image).delete()
+                product.image = request.FILES['image']
+                product.thumbnail = request.FILES['image']
+                product.seller = request.user.seller
+                product.slug = slugify(product.title)
+                product.generate_ref()
+                product.save()
+                images = request.FILES.getlist('more_images')
+                if images:
+                    for image in images:
+                        image = ProductImage(image=image, product=product)
+                        image.save()
+                messages.success(request, 'Your artwork is now pending review!')
+            else:
+                messages.error(request,"The price must be between €1 and €9999!")
+                return render(request,'updateProduct.html',{'form':form,'more_images':more_images})
+            return redirect('dashboard')
+    else:
+        form = ProductForms(instance=getProduct)
+        more_images = ProductImageForm()
+    return render(request,'updateProduct.html',{'form':form,'more_images':more_images})
+
+@login_required(login_url='Login_seller')
+def deleteProduct(request , primaryKey):
+    getProductToDelete = get_object_or_404(Product,pk=primaryKey)
+    try:
+        os.remove("C:/Users/Snof_Bo/Desktop/Enviroment_Python/productlab/media/uploads/"+getProductToDelete.image.name)
+        os.remove("C:/Users/Snof_Bo/Desktop/Enviroment_Python/productlab/media/"+getProductToDelete.image.name)
+    except Exception as error :
+        print(error)
+        pass
+    getProductToDelete.delete()
+    return redirect('dashboard')
