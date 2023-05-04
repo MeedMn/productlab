295ceb44f3957f840075073f17bde3cf7119ac2c
Mohamed Menfalouti <mohamedmenfalouti1@gmail.com>
2023-05-04 12:48:55 +0100

    adding some styles to the form addProduct

--- apps/product/ProductForms.py
+++ apps/product/ProductForms.py
@@ -3,10 +3,14 @@ from .models import Product
 from django.forms import ModelForm
 
 class ProductForms(ModelForm):
-	price = forms.FloatField(widget=forms.NumberInput(attrs={'min':0,'max':9999}))
-	class Meta:
-		model = Product
-		fields = ('category','image','title','description','price','qte')
+    price = forms.FloatField(widget=forms.NumberInput(attrs={'min':0,'max':9999}))
+    class Meta:
+        model = Product
+        fields = ('category','image','title','description','price','qte')
+        widgets = {
+            'category': forms.Select(attrs={'class': 'form-select'}),
+            'image': forms.FileInput(attrs={'class': 'form-control'})
+        }
 
 class ProductImageForm(forms.Form):
 	more_images = forms.ImageField(widget=forms.ClearableFileInput(attrs={'multiple':True}))
