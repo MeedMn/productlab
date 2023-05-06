957edbe43df1ab70bbafcf3d8b8cfbe862888ab3
Mohamed Menfalouti <mohamedmenfalouti1@gmail.com>
2023-05-06 13:55:41 +0100

    adding reset password and receive email of link

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -13,4 +13,7 @@ urlpatterns = [
     path('account_settings/',views.account_settings,name = 'account_settings'),
     path('logout/',auth_views.LogoutView.as_view(),name='logout'),
     path('login/',auth_views.LoginView.as_view(template_name='Login_user.html'),name='login'),
+    path('password_reset/',auth_views.PasswordResetView.as_view(template_name="password_reset_form.html",success_url= reverse_lazy ("password_reset_done"),extra_email_context={'domain':'127.0.0.1:8000','site_name':"ProductLab"}),name="password_reset"),
+    path('password_reset/done/',auth_views.PasswordResetDoneView.as_view(template_name="password_reset_done.html"),name="password_reset_done"),
+    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html',success_url= reverse_lazy ("loginuser")),name='password_reset_confirm'),
 ]
